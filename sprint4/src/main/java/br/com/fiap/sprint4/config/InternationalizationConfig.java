48ff902696ed6d29ac162fa207389258c162d72d
Luna Faustino <lu.faustino2000@gmail.com>
2025-05-15 23:44:17 -0300

    Dando continuidade na internacionalização

--- sprint4/src/main/java/br/com/fiap/sprint4/config/InternationalizationConfig.java
+++ sprint4/src/main/java/br/com/fiap/sprint4/config/InternationalizationConfig.java
@@ -1,7 +1,9 @@
 package br.com.fiap.sprint4.config;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,6 +14,14 @@ import java.util.Locale;
 
 @Configuration
 public class InternationalizationConfig implements WebMvcConfigurer {
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
 
     @Bean
     public LocaleResolver localeResolver() {
